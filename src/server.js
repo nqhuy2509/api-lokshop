a4865077db0484a58b7f497404e5ae44d7d6697e
Nguyễn Quan Huy <64772864+nqhuy2509@users.noreply.github.com>
2023-04-24 23:46:18 +0700

    fix cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 
 // Config middleware
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
 
 app.use(require('./routes/admin.route'))
 app.use(require('./routes/product.route'))
