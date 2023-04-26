a798d25be5b70c0fb8e02beafb70cfde09385b28
nqhuy2509 <nqhuy250901@gmail.com>
2023-04-26 17:17:40 +0700

    update categories

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,65 +1,73 @@
+const { PrismaClient } = require('@prisma/client');
+const { BadRequestExeption, SuccessResponse, BadGatewayExeption } = require('../utils/response');
+const { uploadToS3 } = require('../middleware/upload');
+const message = require('../constants/message');
+const { BAD_GATEWAY } = require('../constants/statusCode');
 
-const {PrismaClient} = require('@prisma/client')
-const { BadRequestExeption, SuccessResponse, BadGatewayExeption } = require('../utils/response')
-const { uploadToS3 } = require('../middleware/upload')
-const message = require('../constants/message')
-const { BAD_GATEWAY } = require('../constants/statusCode')
+const prisma = new PrismaClient();
 
-const prisma = new PrismaClient
+const addNewProduct = async (req, res) => {
+	const { name, description, price, categoryId } = req.body;
+	const image = req.file.buffer;
+	const imageName = req.file.originalname;
 
-const addNewProduct = async (req,res)=>{
-    const {name,description, price,categoryId} = req.body
-    const image = req.file.buffer
-    const imageName = req.file.originalname
+	if (!name || !price || !categoryId) {
+		return BadRequestExeption(res, 'Missing require parameter');
+	}
 
-    if(!name || !price || !categoryId){
-        return BadRequestExeption(res, "Missing require parameter")
-    }
+	const existCategory = await prisma.productCategory.findUnique({ where: { id: categoryId } });
 
-    const existCategory = await prisma.productCategory.findUnique({where :{id: categoryId}})
+	if (!existCategory) {
+		return BadRequestExeption(res, 'Category is not found');
+	}
 
-    if(!existCategory){
-        return BadRequestExeption(res, "Category is not found")
-    }
+	try {
+		const s3Response = await uploadToS3(image, imageName);
+		const newProduct = await prisma.product.create({
+			data: {
+				name,
+				description,
+				price,
+				image: s3Response,
+				categoryId,
+			},
+		});
 
-    try {
-        const s3Response = await uploadToS3(image,imageName)
-        const newProduct = await prisma.product.create({
-            data: {
-                name,
-                description,
-                price,
-                image: s3Response,
-                categoryId
-            }
-        })
+		return SuccessResponse(res, message.product.CREATE_SUCCESS, newProduct);
+	} catch (error) {
+		console.log(error);
+		return BadGatewayExeption(res, message.SOMETHING_WRONG, error);
+	}
+};
 
-        return SuccessResponse(res, message.product.CREATE_SUCCESS, newProduct)
-    } catch (error) {
-        console.log(error);
-        return BadGatewayExeption(res, message.SOMETHING_WRONG, error)
-    }
-}
+const getAllProduct = async (req, res) => {
+	try {
+		const products = await prisma.product.findMany({
+			include: {
+				category: {
+					select: {
+						name: true,
+					},
+				},
+			},
+		});
+		return SuccessResponse(res, message.REQUEST_SUCCESS, products);
+	} catch (error) {
+		return BAD_GATEWAY(res, message.SOMETHING_WRONG, error);
+	}
+};
 
-const getAllProduct = async (req,res) =>{
-    try {
-        const products = await prisma.product.findMany({
-            include: {
-                category: {
-                    select: {
-                        name: true
-                    }
-                }
-            }
-        })
-        return SuccessResponse(res,message.REQUEST_SUCCESS, products)
-    } catch (error) {
-        return BAD_GATEWAY(res,message.SOMETHING_WRONG, error)
-    }
-
-}
+const getAllCategory = async (req, res) => {
+	try {
+		const categories = await prisma.productCategory.findMany({});
+        return SuccessResponse(res,message.REQUEST_SUCCESS, categories)
+	} catch (error) {
+		return BAD_GATEWAY(res, message.SOMETHING_WRONG, error);
+	}
+};
 
 module.exports = {
-    addNewProduct,
-    getAllProduct
-}
+	addNewProduct,
+	getAllProduct,
+    getAllCategory
+};
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -6,6 +6,8 @@ const router = require('express').Router()
 
 const BASE_URL = "/api/v1/product/"
 
+router.get(BASE_URL+'categories', controller.getAllCategory)
+
 router.post(BASE_URL, authenticate, upload.single('image'), controller.addNewProduct)
 router.get(BASE_URL,controller.getAllProduct)
 
